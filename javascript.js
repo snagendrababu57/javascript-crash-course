5e22b911a8b29dec2247646400228cc1781b5244
snagendrababu57 <snagendrababu57@gmail.com>
2023-04-06 16:29:56 +0530

    javascript crash course

---
+++ javascript.js
@@ -0,0 +1,14 @@
+//log aststment console.log()
+console.log('hello from script')
+
+// variables
+
+let age = 25
+console.log(age)
+
+const salary=8000
+console.log(salary)
+
+let sum = 0
+sum = 10
+console.log(sum)
